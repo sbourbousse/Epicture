5dec99ec48d3c6e2b92ede0816e3cabc6e9a3b79
Sylvain <sbourbousse@gmail.com>
2021-03-22 11:49:31 +0100

    Create static home page 🖼

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PostComponent } from '../post/post.component';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
+  templateUrl: 'tab1.page.html',  
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
