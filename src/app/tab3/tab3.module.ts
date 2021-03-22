7b679efbc17c5fdbd365ce0fe079fdd7acb10d8f
Sylvain <sbourbousse@gmail.com>
2021-03-22 17:35:13 +0100

    My account page 🧔

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -8,6 +8,9 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
+import { PostComponentModule } from '../post/post.component.module';
+
+
 @NgModule({
   imports: [
     IonicModule,
@@ -16,6 +19,7 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
+    PostComponentModule
   ],
   declarations: [Tab3Page]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  currentTab="my-posts"
 
   constructor() {}
+
+  changeTab(newTab: string) {
+    this.currentTab = newTab;
+  }
 
 }
